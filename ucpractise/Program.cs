3dec0e4f23fa7650470a8bccbdbe99196a1a4d9e
fox <pipkas232@gmail.com>
2024-09-11 17:00:20 +0400

    v0.3 Добавьте файлы проекта.

--- ucpractise/Program.cs
+++ ucpractise/Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Identity.Data;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+//string connection = "Server=(localdb)\\mssqllocaldb;Database=applicationdb;Trusted_Connection=True;";
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -12,6 +21,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseDefaultFiles();
 
 app.UseStaticFiles();
 
@@ -19,4 +29,63 @@ app.MapRazorPages();
 
 
 
+
+app.MapGet("/api/register", async (ApplicationContext db) => await db.Users.ToListAsync());
+
+var err = 0;
+/*app.UseWhen(context => context.Request.Path == "/api/register",
+    appBuilder =>
+    {
+        appBuilder.Run(async context =>
+        {
+            var UserData = await context.Request.ReadFromJsonAsync<User>(); 
+
+        }
+            );
+    }
+    );*/
+
+app.MapPost("/api/register", async (User userData, ApplicationContext db) =>
+{
+    // получаем пользователя по login
+    var user = await db.Users.FirstOrDefaultAsync(u => u.Login == userData.Login);
+
+    // если не найден, отправляем статусный код и сообщение об ошибке
+    if (user != null) return Results.NotFound(new { message = "Пользователь существует" }); 
+
+    // если пользователь найден, изменяем его данные и отправляем обратно клиенту
+    await db.Users.AddAsync(userData);
+    await db.SaveChangesAsync();
+    return Results.Json(userData);
+});
+
+
+
+
+
 app.Run();
+public class User
+{
+    public int Id { get; set; }
+   public String Login { get; set; } = "";
+   public string Password { get; set; } = "";
+}
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Login = "bob", Password = "1234" },
+                new User { Id = 2, Login = "bib", Password = "1234" },
+                new User { Id = 3, Login = "bab", Password = "1234" }
+        );
+    }
+}
---
+++ ucpractise/wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+
+document.getElementById("reg").addEventListener("click", async () => {
+
+    const login = document.getElementById("login").value;
+    const pass = document.getElementById("passwor").value;
+    const cp = document.getElementById("confpassword").value;
+    const err = document.getElementById("err");
+    if (cp != pass)
+        // await reg(login, pass);
+        err.style.visibility = "visible";
+   // else
+        
+    reset();
+});
