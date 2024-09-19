b07be7bd812e851e8a0a119703fa1767d8933b5e
fox <pipkas232@gmail.com>
2024-09-19 17:52:35 +0400

    v0.3

--- ucpractise/Program.cs
+++ ucpractise/Program.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Identity.Data;
 using System.Reflection.Metadata;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 //string connection = "Server=(localdb)\\mssqllocaldb;Database=applicationdb;Trusted_Connection=True;";
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
-
+builder.Services.AddDbContext<NewsContext>(options => options.UseSqlServer(connection));
 // Add services to the container.
 builder.Services.AddRazorPages();
 
@@ -32,18 +33,33 @@ app.MapRazorPages();
 
 app.MapGet("/api/register", async (ApplicationContext db) => await db.Users.ToListAsync());
 
-var err = 0;
-/*app.UseWhen(context => context.Request.Path == "/api/register",
+List<News> news = new List<News>();
+
+var contextOptions = new DbContextOptionsBuilder<NewsContext>()
+    .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test;ConnectRetryCount=0")
+    .Options;
+
+
+
+
+using (var context = new NewsContext(contextOptions))
+{
+    news = context.news.ToList();
+}
+
+app.UseWhen(context => context.Request.Path == "/api/news",
     appBuilder =>
     {
         appBuilder.Run(async context =>
         {
-            var UserData = await context.Request.ReadFromJsonAsync<User>(); 
+
+            await context.Response.WriteAsJsonAsync(news);
 
         }
             );
     }
-    );*/
+    );
+
 
 app.MapPost("/api/register", async (User userData, ApplicationContext db) =>
 {
@@ -71,6 +87,15 @@ public class User
    public string Password { get; set; } = "";
 }
 
+public class News
+{
+    public int Id { get; set; }
+
+    //public String img { get; set; } = "";
+    public string Head { get; set; } = "";
+    public String Info { get; set; } = "";
+}
+
 public class ApplicationContext : DbContext
 {
     public DbSet<User> Users { get; set; } = null!;
@@ -79,7 +104,7 @@ public class ApplicationContext : DbContext
     {
         Database.EnsureCreated();   // создаем базу данных при первом обращении
     }
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<User>().HasData(
@@ -89,3 +114,32 @@ public class ApplicationContext : DbContext
         );
     }
 }
+
+
+public class NewsContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+    public NewsContext(DbContextOptions<NewsContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();   // создаем базу данных при первом обращении
+    }
+
+    public DbSet<News> news { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<News>().HasData(
+                new News { Id = 1, Head = "bob", Info = "1234" },
+                new News { Id = 2, Head = "bib", Info = "1234" },
+                new News { Id = 3, Head = "bab", Info = "1234" }
+        );
+    }
+}
+
+/*public class NewsContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test;ConnectRetryCount=0");
+    }
+}*/
